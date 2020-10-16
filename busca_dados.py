3666eb6606f934ea9327cd0f821dec2381f73586
Gabrielmarcial <61856695+Gabrielmarcial@users.noreply.github.com>
2020-10-16 10:54:32 -0300

    Update busca_dados.py

--- busca_dados.py
+++ busca_dados.py
@@ -2,7 +2,7 @@ from selenium.webdriver import Chrome
 import time
 # Abrindo o navegador
 url = 'http://www.snirh.gov.br/hidroweb/serieshistoricas'
-
+regiao = str(input('qual a região:'))
 browser = Chrome()
 browser.get(url)
 
@@ -15,37 +15,61 @@ a[0].click()
 b = browser.find_element_by_xpath("//mat-option[@value='P']//span")
 b.click()
 
-#Escolhendo a Região primeira maneira
+#Escolhendo a Região RIO DE JANEIRO
 
-a[6].click()
-time.sleep(11)
-rg = browser.find_element_by_xpath("//mat-option[@id='mat-option-34']//span")
-
-
-
+#a[6].click()
+#time.sleep(11)
+#rg = browser.find_element_by_xpath("//mat-option[@id='mat-option-34']//span")
 # como tem um elemento logo abaixo do span ele não realiza o click com o comando 'rg.click()'
 # para resolver o problema foi usado o comando abaixo
-browser.execute_script('arguments[0].click()', rg)
+#browser.execute_script('arguments[0].click()', rg)
 
+# Escolhendo uma Região em especifico
+a[6].click()
+time.sleep(5)
+
+#regiao = str(input('qual a região:'))
+
+rgs = browser.find_elements_by_tag_name('mat-option')
+
+for c in range(0,len(rgs)):
+    if regiao == rgs[c].text:
+        #rgs[c].click()
+        browser.execute_script('arguments[0].click()', rgs[c])
+        break
+
+time.sleep(4)
 # Consultando os dados
 bot = browser.find_element_by_xpath("//button[@class='mat-flat-button mat-primary']//span")
 bot.click()
+
 
 #criando o dataframe
 import pandas as pd
 dados = pd.DataFrame(columns= ['Código','Nome_da_Estação', 'Estado',' Município',
                                'Responsável_pela_Estação'  ,' Latitude_da_Estação ', ' Longitude_da_Estação'])
 
+
+# selecionando 100 opções
+seli1=browser.find_element_by_class_name('mat-paginator-page-size-select')
+seli1.click()
+time.sleep(2)
+seli = browser.find_elements_by_tag_name('mat-option')
+seli[3].click()
+
+
+# Selecionado o número de paginas
+pagnum1 = browser.find_element_by_class_name('mat-paginator-range-label')
+pagnum2 = str(pagnum1.text)
+pagnum2.split('/')
+pagnum3 = int(pagnum2.split('/')[1])
+numero_paginas = int(pagnum3/100)
+
+
+
 #coletando os dados pagina por pagina
 
-for pag in range(0,194):
-
-    # selecionando 100 opções
-    seli1=browser.find_element_by_class_name('mat-paginator-page-size-select')
-    seli1.click()
-    time.sleep(2)
-    seli = seli1.find_elements_by_tag_name('mat-option')
-    seli[3].click()
+for pag in range(0,numero_paginas+1):
 
     # clicando nos links dos com os dados
     time.sleep(3)
